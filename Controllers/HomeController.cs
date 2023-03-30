d50e8a9e174502cd921a5c6aeef4fe078fb04b67
APTOL301956 <APTOL301956@APOLBO03-176>
2023-03-30 16:06:24 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using JQueryDataTable.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JQueryDataTable.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index1()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult AjaxMethod()
+        {
+            NorthwindEntities entities = new NorthwindEntities();
+            List<Customer> customers = (from customer in entities.Customers select customer).ToList();
+            return Json(customers);
+        }
+    }
+    }
